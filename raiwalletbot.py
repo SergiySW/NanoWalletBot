297e8162e91d1dc70bdd91b7ee5ad971f71747b8
SergiySW <sergiysw@gmail.com>
2017-06-18 15:24:36 +0300

    Better accounts list for Android

--- raiwalletbot.py
+++ raiwalletbot.py
@@ -418,19 +418,10 @@ def account_text(bot, update, list = False):
 			extra_array.append(extra_account[3])
 		if (len(extra_accounts) > 0):
 			balances = accounts_balances(extra_array)
-		extra = ''
-		if (list is not False):
-			extra = '*0.* {0} - {1} XRB (Mrai)'.format(r.replace("xrb_", "xrb\_"), mrai_text(balance))
-		num = 0
 		hide = mysql_select_hide(user_id)
+		num = 0
 		for extra_account in extra_accounts:
 			num = num + 1
-			if ((num <= 3) and (list is False) and (hide == 0)):
-				extra = '{0}\n*{1}.* {2} - {3} XRB (Mrai)  /{4} {1}'.format(extra, extra_account[2], extra_account[3].replace("xrb_", "xrb\_"), mrai_text(balances[extra_account[3]]), lang_text('send_from_command', lang_id).encode("utf8").replace("_", "\_"))
-			elif (list is not False):
-				extra = '{0}\n*{1}.* {2} - {3} XRB (Mrai)'.format(extra, extra_account[2], extra_account[3].replace("xrb_", "xrb\_"), mrai_text(balances[extra_account[3]]))
-				if (num == 1):
-					extra = '{0}  /{1} 1'.format(extra, lang_text('send_from_command', lang_id).encode("utf8").replace("_", "\_"))
 			total_balance = total_balance + balances[extra_account[3]]
 		# price
 		price = mysql_select_price()
@@ -442,8 +433,18 @@ def account_text(bot, update, list = False):
 		btc_balance = ('%.8f' % (btc_price * total_balance))
 		# price
 		if (list is not False):
-			text = 'Total: *{0} XRB (Mrai)*\n~ {1} BTC\n\n{2}\n/{3}'.format(mrai_text(total_balance), btc_balance, extra, lang_text('account_add', lang_id).encode("utf8").replace("_", "\_"))
+			text = 'Total: *{0} XRB (Mrai)*\n~ {1} BTC\n/{3}'.format(mrai_text(total_balance), btc_balance, '', lang_text('account_add', lang_id).encode("utf8").replace("_", "\_"))
 			message_markdown(bot, chat_id, text)
+			sleep(1)
+			message_markdown(bot, chat_id, '*0.* {0} XRB (Mrai)'.format(mrai_text(balance)))
+			sleep(1)
+			message_markdown(bot, chat_id, '*{0}*'.format(r))
+			sleep(1)
+			for extra_account in extra_accounts:
+				message_markdown(bot, chat_id, '*{0}.* {1} XRB (Mrai)  /{2} {0}'.format(extra_account[2], mrai_text(balances[extra_account[3]]), lang_text('send_from_command', lang_id).encode("utf8").replace("_", "\_")))
+				sleep(1)
+				text_reply(update, extra_account[3])
+				sleep(1)
 		else:
 			if ((balance == 0) and (list is False)):
 				text = lang_text('account_balance_zero', lang_id).format(faucet_url, r)
@@ -460,12 +461,23 @@ def account_text(bot, update, list = False):
 			message_markdown(bot, chat_id, '*{0}*'.format(r))
 			sleep(1)
 			if ((num > 3) and (hide == 0)):
-				message_markdown(bot, chat_id, lang_text('account_history', lang_id).encode("utf8").format(r, account_url, faucet_url, extra).replace(lang_text('account_add', lang_id).encode("utf8").replace("_", "\_"), lang_text('account_list', lang_id).encode("utf8").replace("_", "\_"))) # full accounts list
+				message_markdown(bot, chat_id, lang_text('account_history', lang_id).encode("utf8").format(r, account_url, faucet_url, '').replace(lang_text('account_add', lang_id).encode("utf8").replace("_", "\_"), lang_text('account_list', lang_id).encode("utf8").replace("_", "\_"))) # full accounts list
 			elif (hide == 1):
-				message_markdown(bot, chat_id, lang_text('account_history', lang_id).encode("utf8").format(r, account_url, faucet_url, extra).replace(lang_text('account_add', lang_id).encode("utf8").replace("_", "\_"), lang_text('account_list', lang_id).encode("utf8").replace("_", "\_")).replace(lang_text('accounts_hide', lang_id).encode("utf8").replace("_", "\_"), lang_text('accounts_expand', lang_id).encode("utf8").replace("_", "\_"))) # hide-expand
+				message_markdown(bot, chat_id, lang_text('account_history', lang_id).encode("utf8").format(r, account_url, faucet_url, '').replace(lang_text('account_add', lang_id).encode("utf8").replace("_", "\_"), lang_text('account_list', lang_id).encode("utf8").replace("_", "\_")).replace(lang_text('accounts_hide', lang_id).encode("utf8").replace("_", "\_"), lang_text('accounts_expand', lang_id).encode("utf8").replace("_", "\_"))) # hide-expand
 			else:
-				message_markdown(bot, chat_id, lang_text('account_history', lang_id).encode("utf8").format(r, account_url, faucet_url, extra))
+				message_markdown(bot, chat_id, lang_text('account_history', lang_id).encode("utf8").format(r, account_url, faucet_url, ''))
 			sleep(1)
+			# list
+			if (hide == 0):
+				n = 0
+				for extra_account in extra_accounts:
+					n = n + 1
+					if (n <= 3):
+						message_markdown(bot, chat_id, '*{0}.* {1} XRB (Mrai)  /{2} {0}'.format(extra_account[2], mrai_text(balances[extra_account[3]]), lang_text('send_from_command', lang_id).encode("utf8").replace("_", "\_")))
+						sleep(1)
+						text_reply(update, extra_account[3])
+						sleep(1)
+			# list
 			#bot.sendPhoto(chat_id=update.message.chat_id, photo=open('{1}{0}.png'.format(r, qr_folder_path), 'rb'), caption=r)
 			try:
 				bot.sendPhoto(chat_id=update.message.chat_id, photo=open('{1}{0}.png'.format(r, qr_folder_path), 'rb'))
