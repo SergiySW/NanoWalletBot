a5c47c9a0e67be76de133a7a225f7d5d9db4e5eb
SergiySW <sergiysw@gmail.com>
2017-09-05 21:46:49 +0300

    Delay to check balance (workaround)

--- raiwalletbot.py
+++ raiwalletbot.py
@@ -533,6 +533,7 @@ def account_text(bot, update, list = False):
 			custom_keyboard(bot, chat_id, lang_text('language_keyboard', 'common'), lang_text('language_selection', 'common'))
 			try:
 				welcome = rpc_send(wallet, welcome_account, r, raw_welcome_amount)
+				sleep(0.5) # workaround
 				new_balance = account_balance(welcome_account)
 				mysql_update_balance(welcome_account, new_balance)
 				mysql_update_frontier(welcome_account, welcome)
@@ -725,6 +726,7 @@ def send_callback(bot, update, args, from_account = 0):
 							else:
 								fee = send_hash
 							# FEELESS
+							sleep(0.5) # workaround
 							new_balance = account_balance(account)
 							if (from_account == 0):
 								mysql_update_balance(account, new_balance)
@@ -752,6 +754,7 @@ def send_callback(bot, update, args, from_account = 0):
 							receive(destination, send_hash)
 						else:
 							logging.warn('Transaction FAILURE! Account {0}'.format(account))
+							sleep(0.5) # workaround
 							new_balance = account_balance(account)
 							lang_keyboard(lang_id, bot, chat_id, lang_text('send_tx_error', lang_id).format(mrai_text(new_balance)))
 							unlock(wallet, wallet_password) # try to unlock wallet
@@ -810,6 +813,7 @@ def send_all_callback(bot, update):
 					send_hash = '00000000000000000000000000000000000000000000000000000000000000'
 					logging.exception("message")
 				if (('000000000000000000000000000000000000000000000000000000000000000' not in send_hash) and ('locked' not in send_hash)):
+					sleep(0.5) # workaround
 					new_balance = account_balance(account)
 					mysql_update_balance_extra(account, new_balance)
 					mysql_update_frontier_extra(account, send_hash)
@@ -1026,6 +1030,7 @@ def send_finish(bot, update):
 				else:
 					fee = send_hash
 				# FEELESS
+				sleep(0.5) # workaround
 				new_balance = account_balance(account)
 				if (len(extra_account) > 0):
 					mysql_update_balance_extra(account, new_balance)
@@ -1053,6 +1058,7 @@ def send_finish(bot, update):
 				receive(destination, send_hash)
 			else:
 				logging.warn('Transaction FAILURE! Account {0}'.format(account))
+				sleep(0.5) # workaround
 				new_balance = account_balance(account)
 				lang_keyboard(lang_id, bot, chat_id, lang_text('send_tx_error', lang_id).format(mrai_text(new_balance)))
 				unlock(wallet, wallet_password) # try to unlock wallet
