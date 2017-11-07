12ea653966721ae2921d01dc29ab87050c4e6a29
SergiySW <sergiysw@gmail.com>
2017-11-08 01:26:42 +0300

    Adding BitFlip prices

--- prices.py
+++ prices.py
@@ -86,6 +86,32 @@ def bitgrail():
 	mysql_set_price(2, last_price, high_price, low_price, ask_price, bid_price, volume, btc_volume)
 
 
+def bitflip():
+	http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
+	url = 'https://api.bitflip.cc/method/market.getOHLC'
+	json_data = json.dumps({"version": "1.0", "pair": "XRB:BTC"})
+	response = http.request('POST', url, body=json_data, headers={'Content-Type': 'application/json'}, timeout=20.0)
+	json_bitfilp = json.loads(response.data)
+	json_array = json_bitfilp[1]
+	last_price = int(float(json_array['close']) * (10 ** 8))
+	high_price = int(float(json_array['high']) * (10 ** 8))
+	low_price = int(float(json_array['low']) * (10 ** 8))
+	volume = int(float(json_array['volume']))
+	btc_volume = 0
+	
+	url = 'https://api.bitflip.cc/method/market.getRates'
+	json_data = json.dumps({"version": "1.0", "pair": "XRB:BTC"})
+	response = http.request('POST', url, body=json_data, headers={'Content-Type': 'application/json'}, timeout=20.0)
+	json_bitfilp = json.loads(response.data)
+	json_array = json_bitfilp[1]
+	for pair in json_array:
+		if (pair['pair'] in 'XRB:BTC'):
+			ask_price = int(float(pair['sell']) * (10 ** 8))
+			bid_price = int(float(pair['buy']) * (10 ** 8))
+	
+	mysql_set_price(3, last_price, high_price, low_price, ask_price, bid_price, volume, btc_volume)
+
+
 def prices_above_below(bot, user_id, price, exchange, above):
 	lang_id = mysql_select_language(user_id)
 	btc_price = ('%.8f' % (float(price) / (10 ** 8)))
@@ -113,21 +139,27 @@ def price_check():
 	users_high = mysql_select_price_high()
 	price_high_bitgrail = max(int(price[1][0]), int(price[1][4]))
 	price_high_mercatox = max(int(price[0][0]), int(price[0][4]))
+	price_high_bitflip = max(int(price[2][0]), int(price[2][4]))
 	for user in users_high:
 		if ((price_high_bitgrail >= int(user[1])) and ((int(user[2]) == 0) or (int(user[2]) == 1))):
 			prices_above_below(bot, user[0], price_high_bitgrail, "BitGrail.com", 1)
 		elif ((price_high_mercatox >= int(user[1])) and ((int(user[2]) == 0) or (int(user[2]) == 2))):
 			prices_above_below(bot, user[0], price_high_mercatox, "Mercatox.com", 1)
+		elif ((price_high_bitflip >= int(user[1])) and ((int(user[2]) == 0) or (int(user[2]) == 3))):
+			prices_above_below(bot, user[0], price_high_bitflip, "BitFlip.cc", 1)
 	
 	# check if lower
 	users_low = mysql_select_price_low()
 	price_low_bitgrail = min(int(price[1][0]), int(price[1][3]))
 	price_low_mercatox = min(int(price[0][0]), int(price[0][3]))
+	price_low_bitflip = min(int(price[2][0]), int(price[2][3]))
 	for user in users_low:
 		if ((price_low_bitgrail <= int(user[1])) and ((int(user[2]) == 0) or (int(user[2]) == 1))):
 			prices_above_below(bot, user[0], price_low_bitgrail, "BitGrail.com", 0)
 		elif ((price_low_mercatox <= int(user[1])) and ((int(user[2]) == 0) or (int(user[2]) == 2))):
 			prices_above_below(bot, user[0], price_low_mercatox, "Mercatox.com", 0)
+		elif ((price_low_bitflip <= int(user[1])) and ((int(user[2]) == 0) or (int(user[2]) == 3))):
+			prices_above_below(bot, user[0], price_low_bitflip, "BitFlip.cc", 0)
 
 def prices_usual():
 	try:
@@ -142,6 +174,14 @@ def prices_usual():
 			bitgrail()
 		except:
 			time.sleep(1) # even BitGrail can fail
+	try:
+		bitflip()
+	except:
+		time.sleep(5)
+		try:
+			bitflip()
+		except:
+			time.sleep(1)
 	
 	price_check()
 
