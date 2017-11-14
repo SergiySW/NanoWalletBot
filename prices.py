1feed681cf3d30559e5fe554f39c6d8cdc1335ba
SergiySW <sergiysw@gmail.com>
2017-11-14 10:32:28 +0300

    Removing BitFlip from monitoring

--- prices.py
+++ prices.py
@@ -139,27 +139,27 @@ def price_check():
 	users_high = mysql_select_price_high()
 	price_high_bitgrail = max(int(price[1][0]), int(price[1][4]))
 	price_high_mercatox = max(int(price[0][0]), int(price[0][4]))
-	price_high_bitflip = max(int(price[2][0]), int(price[2][4]))
+	#price_high_bitflip = max(int(price[2][0]), int(price[2][4]))
 	for user in users_high:
 		if ((price_high_bitgrail >= int(user[1])) and ((int(user[2]) == 0) or (int(user[2]) == 1))):
 			prices_above_below(bot, user[0], price_high_bitgrail, "BitGrail.com", 1)
 		elif ((price_high_mercatox >= int(user[1])) and ((int(user[2]) == 0) or (int(user[2]) == 2))):
 			prices_above_below(bot, user[0], price_high_mercatox, "Mercatox.com", 1)
-		elif ((price_high_bitflip >= int(user[1])) and ((int(user[2]) == 0) or (int(user[2]) == 3))):
-			prices_above_below(bot, user[0], price_high_bitflip, "BitFlip.cc", 1)
+		#elif ((price_high_bitflip >= int(user[1])) and ((int(user[2]) == 0) or (int(user[2]) == 3))):
+		#	prices_above_below(bot, user[0], price_high_bitflip, "BitFlip.cc", 1)
 	
 	# check if lower
 	users_low = mysql_select_price_low()
 	price_low_bitgrail = min(int(price[1][0]), int(price[1][3]))
 	price_low_mercatox = min(int(price[0][0]), int(price[0][3]))
-	price_low_bitflip = min(int(price[2][0]), int(price[2][3]))
+	#price_low_bitflip = min(int(price[2][0]), int(price[2][3]))
 	for user in users_low:
 		if ((price_low_bitgrail <= int(user[1])) and ((int(user[2]) == 0) or (int(user[2]) == 1))):
 			prices_above_below(bot, user[0], price_low_bitgrail, "BitGrail.com", 0)
 		elif ((price_low_mercatox <= int(user[1])) and ((int(user[2]) == 0) or (int(user[2]) == 2))):
 			prices_above_below(bot, user[0], price_low_mercatox, "Mercatox.com", 0)
-		elif ((price_low_bitflip <= int(user[1])) and ((int(user[2]) == 0) or (int(user[2]) == 3))):
-			prices_above_below(bot, user[0], price_low_bitflip, "BitFlip.cc", 0)
+		#elif ((price_low_bitflip <= int(user[1])) and ((int(user[2]) == 0) or (int(user[2]) == 3))):
+		#	prices_above_below(bot, user[0], price_low_bitflip, "BitFlip.cc", 0)
 
 def prices_usual():
 	try:
--- raiwalletbot.py
+++ raiwalletbot.py
@@ -1150,7 +1150,7 @@ def price_text(bot, update):
 	ask_price_flip = ('%.8f' % (float(price[2][3]) / (10 ** 8)))
 	bid_price_flip = ('%.8f' % (float(price[2][4]) / (10 ** 8)))
 	
-	high_price = ('%.8f' % (max(float(price[0][1]), float(price[1][1])), float(price[0][0])) / (10 ** 8)))
+	high_price = ('%.8f' % (max(float(price[0][1]), float(price[1][1]), float(price[0][0])) / (10 ** 8)))
 	low_price = ('%.8f' % (min(float(price[0][2]), float(price[1][2])) / (10 ** 8)))
 	volume = int(price[0][5]) + int(price[1][5]) + int(price[2][5])
 	volume_btc = ('%.2f' % ((float(price[0][6]) + float(price[1][6])) / (10 ** 8)))
