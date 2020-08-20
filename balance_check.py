e3d0262c7eab5aec920f27b48fcfb2616b6c00d0
Sergey Kroshnin <sergiysw@gmail.com>
2020-08-20 16:05:48 +0300

    Improve balance check

--- balance_check.py
+++ balance_check.py
@@ -32,6 +32,8 @@ from common_rpc import *
 # balances check
 def balance_check():
 	time_start = int(time.time())
+	count = 0
+	diff_summ = 0
 	# list from MySQL
 	accounts_list = mysql_select_accounts_list()
 	accounts_list_extra = mysql_select_accounts_list_extra()
@@ -39,7 +41,10 @@ def balance_check():
 	for account in accounts_list:
 		balance = int(account_balance(account[1]))
 		mysql_balance = int(account[3])
-		if ((balance - mysql_balance) > 999999):
+		diff = abs(balance - mysql_balance)
+		diff_summ = diff_summ + diff
+		if (diff > 0):
+			count = count + 1
 			print(account[1])
 			print('{0} {1}'.format(mysql_balance, balance))
 			
@@ -48,12 +53,16 @@ def balance_check():
 	for account in accounts_list_extra:
 		balance = int(account_balance(account[1]))
 		mysql_balance = int(account[3])
-		if ((balance - mysql_balance) > 999999):
+		diff = abs(balance - mysql_balance)
+		diff_summ = diff_summ + diff
+		if (diff > 0):
+			count = count + 1
 			print(account[1])
 			print('{0} {1}'.format(mysql_balance, balance))
 	
 	time_end = int(time.time())
 	total_time = time_end - time_start
 	print (total_time)
+	print ("{} inconsistent accounts\n{} inconsistent sum (xrb)".format(count, diff_summ))
 
 balance_check()
